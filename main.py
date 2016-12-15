89b9f9b285721e49d12d409b7f02ea54629dc2c8
Liam Hawkins <liamhawkinscu@gmail.com>
2016-12-14 22:22:28 -0500

    Add support for multiple currencies

--- main.py
+++ main.py
@@ -3,11 +3,16 @@ import mail
 import simplejson
 import urllib2
 import time
-
+from config import *
 
 def moniter(m, send_to, high, low):
 
-    req = urllib2.Request("https://data.btcchina.com/data/ticker")
+    if currency == 'CHN':
+        url = 'http://data.btcchina.com/data/ticker'
+    else:
+        url = 'https://blockchain.info/ticker'
+
+    req = urllib2.Request(url)
     opener = urllib2.build_opener()
     last_sent = 0
 
@@ -19,7 +24,17 @@ def moniter(m, send_to, high, low):
             time.sleep(3)
             continue
 
-        price = float(data['ticker']['last'])
+        if currency == 'CHN':
+            price = float(data['ticker']['last'])
+            buy = data['ticker']['buy']
+            sell = data['ticker']['sell']
+            symbol = '￥'
+        else:
+            price = float(data[currency]['last'])
+            buy = str(data[currency]['buy'])
+            sell = str(data[currency]['sell'])
+            symbol = str(data[currency]['symbol'])
+
         if price > high or price < low:
             if time.time() - last_sent > 5 * 60:
                 try:
@@ -30,10 +45,9 @@ def moniter(m, send_to, high, low):
                 except Exception, e:
                     print e
 
-        print "Price: ￥%s  Buy: ￥%s  Sell: ￥%s" % (data['ticker']['last'],
-                    data['ticker']['buy'], data['ticker']['sell'])
+        print("Price: " + symbol + str(price) + " Buy: " + symbol + buy + "  Sell: " + symbol + sell)
         time.sleep(3)
-        
+
 if __name__ == "__main__":
     m = mail.Mail(account, smtp_addr, account, password)
     moniter(m, send_to, high, low)
